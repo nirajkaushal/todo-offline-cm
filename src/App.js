2b29702b1ac7b6b583c906f9c3f0e548cddb746e
Niraj Kaushal <nirajkaushal007@gmail.com>
2021-07-10 14:38:47 +0530

    Setup tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react"
 import "./App.css"
 
 function App() {
-  return <div>Welcome</div>
+  return (
+    <div className="flex justify-center items-center bg-blue-100 w-3/5 h-auto m-auto p-4">
+      Doitoday
+    </div>
+  )
 }
 
 export default App
