6afadf67e6737e6d739278140fb15a405edcf5c6
Niraj Kaushal <nirajkaushal007@gmail.com>
2021-07-10 23:29:44 +0530

    Implement network status

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./App.css"
 import NetworkStatusBar from "./components/NetworkStatusBar"
 import TodoList from "./components/TodoList"
 import TodoHeader from "./components/TodoHeader"
 
 function App() {
+  const [online, setOnline] = useState(true)
+
+  useEffect(() => {
+    setOnline(window.navigator.onLine)
+
+    window.addEventListener("offline", () => {
+      console.log("Offline")
+      setOnline(false)
+    })
+
+    window.addEventListener("online", () => {
+      console.log("Online")
+      setOnline(true)
+    })
+
+    return () => {
+      window.removeEventListener("offline")
+      window.removeEventListener("online")
+    }
+  }, [])
+
   return (
     <div className="flex justify-center p-12 h-auto bg-primary-500">
-      <NetworkStatusBar />
+      <NetworkStatusBar online={online} />
       <div className="w-3/5 rounded-md bg-white p-10">
         <TodoHeader />
         <TodoList />
