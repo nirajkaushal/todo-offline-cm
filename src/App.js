2738463e20a86e83badc7e81ae1a491aeaf89164
Niraj Kaushal <nirajkaushal007@gmail.com>
2021-07-12 12:25:57 +0530

    Fix width issue

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="flex justify-center p-0 md:p-12 min-h-screen h-auto bg-primary-500">
       <NetworkStatusBar />
-      <div className="lg:w-3/5 rounded-sm md:rounded-md bg-white p-2 md:p-10 max-w-2xl box-content">
+      <div className="w-full lg:w-3/5 rounded-sm md:rounded-md bg-white p-2 md:p-10 max-w-2xl box-content">
         <TodoHeader />
         <TodoList />
       </div>
